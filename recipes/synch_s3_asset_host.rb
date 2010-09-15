5348a93b020067d8e8ce908432cb75abd8116a4d
Eric Dennis <e@wb.com>
2010-09-15 09:42:50 -0700

    Fixed load_config task to work with my multi-stage deployments.

--- recipes/synch_s3_asset_host.rb
+++ recipes/synch_s3_asset_host.rb
@@ -169,7 +169,7 @@ namespace :s3_asset_host do
   
   desc "Loads the S3 id and secret from the .yaml file set in CONFIG_FILENAME"
   task :load_config do
-    rails_env = ENV["RAILS_ENV"] || "production"
+    # rails_env = ENV["RAILS_ENV"] || "production"
     s3sync_config = YAML::load(File.open(CONFIG_FILENAME))
     s3sync_config.each do |key, value|
       set(key.downcase.to_sym, value)
